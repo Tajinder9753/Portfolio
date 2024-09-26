c0096a1e0045e5db6b9d4275179c1b2c54c8bc7f
Tajinder <tnijjar2@my.centennialcollege.ca>
2024-09-25 21:10:11 -0400

    Completed services page

--- my-react-app/src/Services.jsx
+++ my-react-app/src/Services.jsx
@@ -1,8 +1,27 @@
 //Services.jsx Tajinder Nijjar, 301059291, Sept 22nd 2024
+import './services.css';
+import service1 from './assets/services1.png';
+import service2 from './assets/services2.png';
+import service3 from './assets/services3.png';
 export default function Services() {
     return (
         <>
-            <p>Services</p>
+            <section className = 'services'>
+                <h2 id = 'serviceTitle'>Services Offered:</h2>
+                <ul className = 'serviceList'>
+                    <li id = 'serviceItem'>Object Oriented Programming (C#, JavaScript, Java)</li>
+                    <li id = 'serviceItem'>General Programming</li>
+                    <li id = 'serviceItem'>Web Development</li>
+                    <li id = 'serviceItem'>Website Design</li>
+                    <li id = 'serviceItem'>Game Programming</li>
+                    <li id = 'serviceItem'>Game Development</li>
+                </ul>
+                <div className = 'pictures'>
+                    <img src = {service1} alt = "OOP image" id = 'serviceImages'></img>
+                    <img src = {service2} alt = "Game Dev image" id = 'serviceImages'></img>
+                    <img src = {service3} alt = "Web dev image" id = 'serviceImages'></img>
+                </div>
+            </section>
         </>
     );
 }
